5d7178f28a517b991cb2b1d8ec506f669dcc2bb9
Ivan Rechkin <r1noffs@gmail.com>
2025-02-25 18:10:33 +0500

    Replace the placeholder repository string

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "r1noff/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
